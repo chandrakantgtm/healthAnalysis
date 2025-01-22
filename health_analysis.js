1f4e0630d85985faff115db91362a0ede1946440
chandrakantgtm <chandrakantgtm.20@gmail.com>
2025-01-22 19:06:23 +0530

    Add js and contact page

---
+++ health_analysis.js
@@ -0,0 +1,107 @@
+// addPatientButton: The button used to add patient data
+const addPatientButton = document.getElementById("addPatient");
+// report: The HTML element where you will see analysis reports displayed
+const report = document.getElementById("report");
+// btnSearch: The variable name of the button which displays the search results when clicked
+const btnSearch = document.getElementById('btnSearch');
+// patients: An empty array named patients is also created to store the collected patient data.
+const patients = [];
+
+// Add Patient Function
+function addPatient() {
+    // Retrieves the patient's details below in the form such as name, gender, age, and condition
+    const name = document.getElementById("name").value;
+    const gender = document.querySelector('input[name="gender"]:checked');
+    const age = document.getElementById("age").value;
+    const condition = document.getElementById("condition").value;
+
+    if (name && gender && age && condition) {
+      patients.push({ name, gender: gender.value, age, condition });
+      resetForm();
+      generateReport();
+    }
+  }
+
+  // Reset Form Function
+  function resetForm() {
+    document.getElementById("name").value = "";
+    document.querySelector('input[name="gender"]:checked').checked = false;
+    document.getElementById("age").value = "";
+    document.getElementById("condition").value = "";
+  }
+
+  // Generate report Function
+  function generateReport() {
+    const numPatients = patients.length;
+    const conditionsCount = {
+      Diabetes: 0,
+      Thyroid: 0,
+      "High Blood Pressure": 0,
+    };
+    const genderConditionsCount = {
+      Male: {
+        Diabetes: 0,
+        Thyroid: 0,
+        "High Blood Pressure": 0,
+      },
+      Female: {
+        Diabetes: 0,
+        Thyroid: 0,
+        "High Blood Pressure": 0,
+      },
+    };
+
+    for (const patient of patients) {
+      conditionsCount[patient.condition]++;
+      genderConditionsCount[patient.gender][patient.condition]++;
+    }
+
+    report.innerHTML = `Number of patients: ${numPatients}<br><br>`;
+    report.innerHTML += `Conditions Breakdown:<br>`;
+    for (const condition in conditionsCount) {
+      report.innerHTML += `${condition}: ${conditionsCount[condition]}<br>`;
+    }
+
+    report.innerHTML += `<br>Gender-Based Conditions:<br>`;
+    for (const gender in genderConditionsCount) {
+      report.innerHTML += `${gender}:<br>`;
+      for (const condition in genderConditionsCount[gender]) {
+        report.innerHTML += `&nbsp;&nbsp;${condition}: ${genderConditionsCount[gender][condition]}<br>`;
+      }
+    }
+  }
+
+addPatientButton.addEventListener("click", addPatient);
+
+// Search Health Condition Function that fetch condition from health_analysis.json
+function searchCondition() {
+    const input = document.getElementById('conditionInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    fetch('health_analysis.json')
+      .then(response => response.json())
+      .then(data => {
+        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+
+        if (condition) {
+          const symptoms = condition.symptoms.join(', ');
+          const prevention = condition.prevention.join(', ');
+          const treatment = condition.treatment;
+
+          resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+          resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="${condition.name}">`;
+
+          resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
+          resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
+          resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+        } else {
+          resultDiv.innerHTML = `"${input}" Condition not found.`;
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
+      });
+  }
+    btnSearch.addEventListener('click', searchCondition);
